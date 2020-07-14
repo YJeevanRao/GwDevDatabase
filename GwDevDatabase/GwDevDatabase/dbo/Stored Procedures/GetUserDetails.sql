236cf11b0cb73ad3193414769e65854595c8746d
Jeevan Rao <yjeevanrao@gmail.com>
2020-07-14 15:03:34 +0530

    Added New DACPAC

---
+++ GwDevDatabase/GwDevDatabase/dbo/Stored Procedures/GetUserDetails.sql
@@ -0,0 +1,14 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE GetUserDetails
+AS
+BEGIN	
+-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT username,PhoneNumber,IsAttrited, CreatedOn, Address from UserDetails
+END
---
+++ GwDevDatabase/GwDevDatabase/dbo/Tables/UserDetails.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[UserDetails] (
+    [ID]          INT           IDENTITY (1, 1) NOT NULL,
+    [UserName]    VARCHAR (150) NULL,
+    [Address]     VARCHAR (250) NULL,
+    [PhoneNumber] VARCHAR (250) NULL,
+    [Password]    VARCHAR (150) NULL,
+    [IsAttrited]  TINYINT       NULL,
+    [CreatedOn]   DATETIME      NULL,
+    [AttritedOn]  DATETIME      NULL,
+    CONSTRAINT [PK_UserDetails] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
