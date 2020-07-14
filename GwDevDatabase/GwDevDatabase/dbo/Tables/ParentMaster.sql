d31586b8600fc7c3091877302a770b9d0d73c5d2
Jeevan Rao <yjeevanrao@gmail.com>
2020-07-14 16:01:59 +0530

    new table added

---
+++ GwDevDatabase/GwDevDatabase/dbo/Tables/ParentMaster.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[ParentMaster]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [StudentId] INT NULL, 
+    [Name] VARCHAR(50) NULL, 
+    [MobileNumber] VARCHAR(50) NULL, 
+    [POC] VARCHAR(50) NULL
+)
